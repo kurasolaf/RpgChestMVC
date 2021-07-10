9312e35e0ea8be03c5839c30d6bbf3e13891a731
olaf kuras <kurasolaf@gmail.com>
2021-07-10 21:45:38 +0200

    CharacterProfile added

---
+++ RpgChestMVC.web/Models/CharacterProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpgChestMVC.web.Models
+{
+    public class CharacterProfile
+    {
+        // Add: item statistic summary, item basic information,
+        // link to generated items (with database)
+
+
+        
+    }
+}
