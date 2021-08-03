d098c847f82449872af15d9d1650a9017c4fa906
olaf kuras <kurasolaf@gmail.com>
2021-08-03 21:26:14 +0200

    struct rarity repaired

--- RpgChestMVC.web/Models/Item.cs
+++ RpgChestMVC.web/Models/Item.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace RpgChestMVC.web.Models
 {
 
-    // Pamiętać o przeniesieniu do DOMAIN do modeli oraz zrobić klucze własne i obce
+    // Pamiętać o przeniesieniu do DOMAIN do modeli oraz zrobić klucze własne i obce w każdym dac ID
     public class Item : IItemBasicStats
     {
         public int Id { get; set; }
