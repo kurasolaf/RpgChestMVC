f817167efedba590491fab44d9c86d7a9d5860cf
olaf kuras <kurasolaf@gmail.com>
2022-02-03 21:57:34 +0100

    Added  last and most complicated prop. for items. "spell' - as To Do

---
+++ RpgChesrMVC.Domain/Model/Spell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgChestMVC.Domain.Model
+{
+    class Spell
+    {
+        //To DO:
+        //-Make Spell creator and add "Spell" to prop. list of items in items
+
+    }
+}
