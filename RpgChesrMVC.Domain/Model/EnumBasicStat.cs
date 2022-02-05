89aa78a5c1d4169aa16bbc713f402dc8589615e2
olaf kuras <kurasolaf@gmail.com>
2022-02-05 16:19:17 +0100

    Added all posib. Item prop.

---
+++ RpgChesrMVC.Domain/Model/EnumBasicStat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgChestMVC.Domain.Model
+{
+    public enum EnumBasicStat
+    {
+        Strength = 1,
+        Agility = 2,
+        Build = 3,
+        Intellect = 4, 
+        Wisdom = 5,
+        Charisma = 6
+
+    }
+}
---
+++ RpgChesrMVC.Domain/Model/EnumSecondaryStat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgChestMVC.Domain.Model
+{
+    public enum EnumSecondaryStat
+    {
+            Endurance = 1,
+            Reflex = 2,
+            Will = 3
+    }
+}
