6642c6bd2de7c3e59f780e8fd79a2e255cd24e5c
olaf kuras <kurasolaf@gmail.com>
2022-03-13 18:33:27 +0100

    Practise with AutoMapper

---
+++ RpgChestMVC.Application/Mapping/IMapFrom.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgChestMVC.Application.Mapping
+{
+    public interface IMapFrom<T>
+    {
+
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+
+    }
+}
