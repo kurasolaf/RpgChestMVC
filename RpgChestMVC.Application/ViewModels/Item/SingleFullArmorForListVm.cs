c5012b01b385df62631b27a2244cb4fe14c49d69
Olaf <kurasolaf@gmail.com>
2022-08-14 23:47:43 +0200

    FullArmorTypes mapping

--- RpgChestMVC.Application/ViewModels/Item/SingleFullArmorForListVm.cs
+++ RpgChestMVC.Application/ViewModels/Item/SingleFullArmorForListVm.cs
@@ -23,8 +23,8 @@ namespace RpgChestMVC.Application.ViewModels.Item
 
         public void Mapping(Profile profile)
         {
-            profile.CreateMap<RpgChestMVC.Domain.Model.FullArmor, SingleFullArmorForListVm>();
-
+            profile.CreateMap<RpgChestMVC.Domain.Model.FullArmor, SingleFullArmorForListVm>()
+            .ForMember(d => d.TypeOfArmor, opt => opt.MapFrom(s => s.TypeOfArmor.ArmorTypes));
 
         }
      
