7bbcd7a555166d36068cc883ee0fa306ad2a228f
Olaf <kurasolaf@gmail.com>
2022-11-29 22:57:23 +0100

    Last test for this project.

--- RpgChestMVC.Application/ViewModels/Item/ListofFullArmorsVm.cs
+++ RpgChestMVC.Application/ViewModels/Item/ListofFullArmorsVm.cs
@@ -1,10 +1,6 @@
 ﻿using AutoMapper;
 using RpgChestMVC.Application.Mapping;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RpgChestMVC.Application.ViewModels.Item
 {
